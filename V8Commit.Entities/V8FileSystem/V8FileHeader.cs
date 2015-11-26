07a59be7766b4954cef42a671ebab435f107812a
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-11-26 23:46:49 +0200

    Added V8FileHeader entity class

---
+++ V8Commit.Entities/V8FileSystem/V8FileHeader.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace V8Commit.Entities.V8FileSystem
+{
+    public class V8FileHeader
+    {
+        private UInt64 _creationDate;
+        private UInt64 _modificationDate;
+        private Int32 _reservedField;
+        private string _fileName;
+
+        public UInt64 CreationDate
+        {
+            get
+            {
+                return _creationDate;
+            }
+
+            set
+            {
+                _creationDate = CreationDate;
+            }
+        }
+        public UInt64 ModificationDate
+        {
+            get
+            {
+                return _modificationDate;
+            }
+
+            set
+            {
+                _modificationDate = ModificationDate;
+            }
+        }
+        public Int32 ReservedField
+        {
+            get
+            {
+                return ReservedField;
+            }
+
+            set
+            {
+                _reservedField = ReservedField;
+            }
+        }
+        public string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+
+            set
+            {
+                _fileName = FileName;
+            }
+        }
+
+        public static Int32 Size()
+        {
+            return 20; // 8 + 8 + 4 http://en.wikipedia.org/wiki/Data_structure_alignment
+        }
+    }
+}
