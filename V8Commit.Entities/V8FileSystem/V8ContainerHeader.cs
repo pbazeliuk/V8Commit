d8d74e113b6b749a6f68ba710a228527af2ee535
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-11-26 23:35:32 +0200

    Added V8ContainerHeader entity class

---
+++ V8Commit.Entities/V8FileSystem/V8ContainerHeader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V8Commit.Entities.V8FileSystem
+{
+    public class V8ContainerHeader
+    {
+
+        private Int32 _refToNextPage;
+        private Int32 _pageSize;
+        private Int32 _pagesCount;
+        private Int32 _reservedField;
+
+        public Int32 RefToNextPage
+        {
+            get
+            {
+                return _refToNextPage;
+            }
+
+            set
+            {
+                _refToNextPage = RefToNextPage;
+            }
+        }
+        public Int32 PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+
+            set
+            {
+                _pageSize = PageSize;
+            }
+        }
+        public Int32 PagesCount
+        {
+            get
+            {
+                return _pagesCount;
+            }
+
+            set
+            {
+                _pagesCount = PagesCount;
+            }
+        }
+        public Int32 ReservedField
+        {
+            get
+            {
+                return _reservedField;
+            }
+
+            set
+            {
+                _reservedField = ReservedField;
+            }
+        }
+
+        public V8ContainerHeader()
+        {
+            _refToNextPage = 0x7FFFFFFF;
+        }
+
+        public static Int32 Size()
+        {
+            return 16; // 4 + 4 + 4 + 4 buffer must exactly 16 byte
+        }
+
+    }
+}
