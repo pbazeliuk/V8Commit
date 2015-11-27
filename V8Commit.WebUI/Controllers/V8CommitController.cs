bd4bb85aabdaa8216629efad8a9201ed8c0a9c88
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-11-28 01:32:11 +0200

    Added some bootstrap template to WebUI project. Updated Readme.MD

---
+++ V8Commit.WebUI/Controllers/V8CommitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V8Commit.WebUI.Controllers
+{
+    public class V8CommitController : Controller
+    {
+        // GET: V8Commit
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
