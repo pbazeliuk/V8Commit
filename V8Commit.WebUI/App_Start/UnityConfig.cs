a26fb699e1d9604469a6c0acdfa6738d324ebdda
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-11-28 13:26:35 +0200

    Added posibility upload files to WebUI

--- V8Commit.WebUI/App_Start/UnityConfig.cs
+++ V8Commit.WebUI/App_Start/UnityConfig.cs
@@ -9,13 +9,20 @@ namespace V8Commit.WebUI
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
+
             // e.g. container.RegisterType<ITestService, TestService>();
-            
+            RegisterTypes(container);
+
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
+
+        public static void RegisterTypes(UnityContainer container)
+        {
+
+        }
+
     }
 }
--- V8Commit.WebUI/Controllers/V8CommitController.cs
+++ V8Commit.WebUI/Controllers/V8CommitController.cs
@@ -8,9 +8,27 @@ namespace V8Commit.WebUI.Controllers
 {
     public class V8CommitController : Controller
     {
-        // GET: V8Commit
+
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload()
+        {
+            for (int i = 0; i < Request.Files.Count; i++)
+            {
+                //HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+                //Use the following properties to get file's name, size and MIMEType
+                //int fileSize = file.ContentLength;
+                //string fileName = file.FileName;
+                //string mimeType = file.ContentType;
+                //System.IO.Stream fileContent = file.InputStream;
+                //To save file, use SaveAs method
+                //file.SaveAs(Server.MapPath("~/") + fileName); //File will be saved in application root
+            }
             return View();
         }
     }
