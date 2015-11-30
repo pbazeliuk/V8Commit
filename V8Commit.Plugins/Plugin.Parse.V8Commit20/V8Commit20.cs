d0d3f960deb0059a7ce897d8f15205c50cbe767c
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-11-30 16:58:41 +0200

    Added V8Commit20 plugin

---
+++ V8Commit.Plugins/Plugin.Parse.V8Commit20/V8Commit20.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Copyright © 2015 Petro Bazeliuk 
+ *
+ * The contents of this file are subject to the terms of one of the following
+ * open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
+ * select the license that you prefer but you may not use this file except in
+ * compliance with one of these Licenses.
+ * 
+ * You can obtain a copy of the Apache 2.0 license at
+ * http://www.opensource.org/licenses/apache-2.0
+ * 
+ * You can obtain a copy of the EPL 1.0 license at
+ * http://www.opensource.org/licenses/eclipse-1.0
+ * 
+ * See the Licenses for the specific language governing permissions and
+ * limitations under the Licenses.
+ *
+ */
+
+using System.ComponentModel.Composition;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+using V8Commit.Entities.V8FileSystem;
+using V8Commit.Plugins;
+using V8Commit.Services.FileV8Services;
+
+namespace Plugin.V8Commit20
+{
+    [Export(typeof(IParsePlugin)),
+        ExportMetadata("Name", "V8Commit20"),
+        ExportMetadata("Author", "Copyright © Petro Bazeliuk 2015"),
+        ExportMetadata("Version", "1.0.0.0")]
+    public class V8Commit20 : IParsePlugin
+    {
+        public void Parse(FileV8Reader fileV8Reader, V8FileSystem fileSystem, string output, int threads = 1)
+        {
+
+        }
+    }
+}
