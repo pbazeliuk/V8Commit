8a06ca76ece5b3b1e8ba8b78426f2c5485641123
Petro Bazeliuk <pbazeliuk@gmail.com>
2015-12-01 12:38:28 +0200

    Changes APP icon

--- V8Commit.Plugins/Plugin.Parse.V8Commit20/V8Commit20.cs
+++ V8Commit.Plugins/Plugin.Parse.V8Commit20/V8Commit20.cs
@@ -1,27 +1,30 @@
 ﻿/**
- * Copyright © 2015 Petro Bazeliuk 
- *
- * The contents of this file are subject to the terms of one of the following
- * open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
- * select the license that you prefer but you may not use this file except in
- * compliance with one of these Licenses.
- * 
- * You can obtain a copy of the Apache 2.0 license at
- * http://www.opensource.org/licenses/apache-2.0
- * 
- * You can obtain a copy of the EPL 1.0 license at
- * http://www.opensource.org/licenses/eclipse-1.0
- * 
- * See the Licenses for the specific language governing permissions and
- * limitations under the Licenses.
- *
- */
+* Copyright © 2015 Petro Bazeliuk 
+*
+* The contents of this file are subject to the terms of one of the following
+* open source licenses: Apache 2.0 or or EPL 1.0 (the "Licenses"). You can
+* select the license that you prefer but you may not use this file except in
+* compliance with one of these Licenses.
+* 
+* You can obtain a copy of the Apache 2.0 license at
+* http://www.opensource.org/licenses/apache-2.0
+* 
+* You can obtain a copy of the EPL 1.0 license at
+* http://www.opensource.org/licenses/eclipse-1.0
+* 
+* See the Licenses for the specific language governing permissions and
+* limitations under the Licenses.
+*
+*/
 
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using V8Commit.Entities.V8FileSystem;
 using V8Commit.Plugins;
 using V8Commit.Services.FileV8Services;
@@ -36,7 +39,7 @@ namespace Plugin.V8Commit20
     {
         public void Parse(FileV8Reader fileV8Reader, V8FileSystem fileSystem, string output, int threads = 1)
         {
-
+            List<IParsePlugin> d =  new
         }
     }
 }
